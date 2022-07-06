2d6e9316bd998905f791b37fd42d2fb0eb7ddeb8
Ian <i.r.butterworth@gmail.com>
2022-07-06 11:48:55 -0400

    kc updates

--- B.jl/src/B.jl
+++ B.jl/src/B.jl
@@ -5,10 +5,15 @@ using Example
 function foo(x::AbstractArray)
     @info "B.jl: A custom logging method for AbstractArray" x
 end
-if using OffsetArrays
+
+if Base.is_package_available(@__MODULE__, :OffsetArrays)
+    using OffsetArrays
+    println("Loading custom OffsetArrays code...")
     function foo(x::OffsetArray)
         @info "B.jl: A custom logging method for OffsetArray" x
     end
+else
+    println("OffsetArrays not installed")
 end
 
 greet(n::String) = Example.hello(n)
