1df404037b30e25adebbfb6c14caa5b3210efe04
KristofferC <kristoffer.carlsson@juliacomputing.com>
2022-07-18 13:52:22 +0200

    add tests with an extra that is a weak dep

--- HasWeakDeps.jl/src/HasWeakDeps.jl
+++ HasWeakDeps.jl/src/HasWeakDeps.jl
@@ -9,11 +9,13 @@ end
 if Base.@hasdep OffsetArrays
     using OffsetArrays
     println("Loading custom OffsetArrays code...")
+    offsetarrays_loaded = true
     function foo(x::OffsetArray)
         @info "HasWeakDeps.jl: A custom logging method for OffsetArray" x
     end
 else
     println("OffsetArrays not installed")
+    offsetarrays_loaded = false
 end
 
 end # module
